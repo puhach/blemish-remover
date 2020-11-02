783e5936a07a59dd34fbb63c20beb003b153f22b
Yaroslav Pugach <pugach.yaroslav2@gmail.com>
2020-11-02 19:28:14 +0200

    Estimate ROI sharpness

--- submission.cpp
+++ submission.cpp
@@ -26,11 +26,13 @@ private:
 
 	static constexpr const char windowName[] = "Blemish Removal";
 	static constexpr int maxUndoQueueLength = 10;
-	static constexpr int blemishSize = 5;	
+	static constexpr int blemishSize = 25;	
 
 	static void onMouse(int event, int x, int y, int flags, void *data);
 
 	void removeBlemish(int x, int y);
+
+	double calcSharpness(const Mat &roi);
 
 	void saveState();
 
@@ -99,49 +101,44 @@ void BlemishRemover::onMouse(int event, int x, int y, int flags, void* data)
 
 void BlemishRemover::removeBlemish(int x, int y)
 {
-	//const Mat &roi = this->imCur(Range(this->imCur.rows-1, 0), Range(0, 20));
-	//cout << roi.size() << endl;
-
 	// In order to be able to remove blemishes from image corners, we need to pad the image
 
 	static constexpr int padding = BlemishRemover::blemishSize / 2;
 	Mat imPadded;
 	copyMakeBorder(this->imCur, imPadded, padding, padding, padding, padding, BORDER_REFLECT | BORDER_ISOLATED);
 
+	// Compute gradients to measure smoothness (since the input image is 8-bit, the derivatives will be truncated)
+	Mat gradX, gradY;
+	Sobel(imPadded, gradX, CV_16S, 1, 0, 3);	// TODO: try using Scharr
+	Sobel(imPadded, gradY, CV_16S, 0, 1, 3);
+	Mat absGrad = abs(gradX) + abs(gradY);
+	//cout << absGrad << endl;
 
 	// The direction arrays aid in calculating the starting row/column of the neighbor regions
 	static constexpr int ndirs = 4;
-	static constexpr int xdir[ndirs] = { -3, -1, +1, -1 }, ydir[ndirs] = { -1, -3, -1, +1 };
+	//static constexpr int xdir[ndirs] = { -3, -1, +1, -1 }, ydir[ndirs] = { -1, -3, -1, +1 };
+	static constexpr int xdir[ndirs] = { -1, 0, +1, 0 }, ydir[ndirs] = { 0, -1, 0, +1 };
 
 	Mat bestPatch;
 	double minSharpness = numeric_limits<double>::max();
 
 	for (int i = 0; i < ndirs; ++i)
 	{
-		int xcorr = static_cast<int>(xdir[i] > 0);
-		int ycorr = static_cast<int>(ydir[i] > 0);
-		int x1 = padding + x + xdir[i] * blemishSize / 2 + xcorr, x2 = x1 + blemishSize;
-		int y1 = padding + y + ydir[i] * blemishSize / 2 + ycorr, y2 = y1 + blemishSize;
+		int nx = padding + x + xdir[i] * blemishSize, ny = padding + y + ydir[i] * blemishSize;
 
-		if (x1 < 0 || x2 > imPadded.cols || y1 < 0 || y2 > imPadded.rows)
+		Range colRange(nx - blemishSize / 2, nx + blemishSize / 2 + 1)
+			, rowRange(ny - blemishSize / 2, ny + blemishSize / 2 + 1);
+
+		if (colRange.start < 0 || colRange.end > imPadded.cols || rowRange.start < 0 || rowRange.end > imPadded.rows)
 			continue;
 
-		//x1 = max(0, x1);
-		//x2 = min(x2, this->imCur.cols);
-		//y1 = max(0, y1);
-		//y2 = min(y2, this->imCur.rows);
-
-		const Mat& roi = imPadded(Range(y1, y2), Range(x1, x2));
-		cout << roi.size() << endl;
-
-		double sharpness = calcSharpness(roi);
+		double sharpness = sum(sum(absGrad(rowRange, colRange)))[0];
 		if (sharpness < minSharpness)
 		{
 			minSharpness = sharpness;
-			bestPatch = roi;
+			bestPatch = imPadded(rowRange, colRange);
 		}
 	}	// for
-
 
 	if (bestPatch.empty())
 	{
@@ -149,47 +146,20 @@ void BlemishRemover::removeBlemish(int x, int y)
 		return;
 	}
 
-	seamlessClone(this->imCur, bestPatch, Mat(bestPatch.size(), CV_8UC1, Scalar(255)), Point(x,y), this->imCur, NORMAL_CLONE);
-
-	/*Range lrange(x - blemishSize/2 - blemishSize, x - blemishSize/2)
-		, rrange(x + blemishSize/2 + 1, x + blemishSize/2 + blemishSize + 1)
-		, urange(x - blemishSize/2, x + blemishSize/2 + 1);
-
-
-	int t1 = lrange.empty();
-	int t2 = lrange.size();*/
-	//static constexpr int dirLen = 4;
-	//static const int dir[dirLen][2] = { {-1, 0}, {0, -1}, {+1, 0}, {0, +1} };
-
-	//static const array<array<int, 2>, 4> dir{ { {-1, 0}, { 0, -1 }, { +1, 0 }, { 0, +1 } } };
-
-	//constexpr int w = BlemishRemover::blemishSize;
-
-	//for (int i = 0; i < dir.size(); ++i)
-	//{
-	//	
-	//	int fromCol = x + dir[i][0] * BlemishRemover::blemishSize/2
-	//	  , toCol = x + dir[i][0] * (BlemishRemover::blemishSize/2 + BlemishRemover::blemishSize)
-	//	  , fromRow = y + dir[i][1] * BlemishRemover::blemishSize/2
-	//	  , toRow = y + dir[i][1] * (BlemishRemover::blemishSize/2 + BlemishRemover::blemishSize);
-
-	//	if (fromCol > toCol)
-	//	{
-	//		swap(fromCol, toCol);
-	//		++fromCol;
-	//		++toCol;
-	//	}
-
-	//	if (fromRow > toRow)
-	//	{
-	//		swap(fromRow, toRow);
-	//		++fromRow;
-	//		++toRow;
-	//	}
-	//}	// for
-	
-	
+	Mat blend;
+	seamlessClone(bestPatch, imPadded, Mat(bestPatch.size(), CV_8UC1, Scalar(255)), Point(padding+x,padding+y), blend, NORMAL_CLONE);	
+	//seamlessClone(bestPatch, this->imCur, Mat(bestPatch.size(), CV_8UC1, Scalar(255)), Point(x, y), blend, NORMAL_CLONE);
+	this->imCur = blend(Rect(padding, padding, this->imCur.cols, this->imCur.rows));
 }	// removeBlemish
+
+//double BlemishRemover::calcSharpness(const Mat &roi) const
+//{
+//	Mat roiGray;
+//	Mat gradx, grady;
+//	Sobel(roi, gradx, CV_64F, 1, 0, 3);
+//	Sobel(roi, grady, CV_64F, 0, 1, 3);
+//	return sum(abs(gradx)) + sum(abs(grady));
+//}	// calcSharpness
 
 void BlemishRemover::saveState()
 {
